f66f5236fce0b0611b2378c0d330722a6557097e
723 <rb.natsumi@gmail.com>
2017-09-11 16:45:46 +0900

    postのtestを実装

--- test/index.js
+++ test/index.js
@@ -1,11 +1,20 @@
 var app = require("../app");
 var request = require("supertest");
 
+var test_json = {};
 describe('コントローラ 正常系 テスト',function(){
   describe('GET /',function(){
     it("ステータスコード200が返ってくる",function(done){
       request(app) 
       .get('/')
+      .expect(200,done);
+    });
+  })
+  describe('POST /',function(){
+    it("ステータスコード200が返ってくる",function(done){
+      request(app)
+      .post('/')
+      .send(test_json)
       .expect(200,done);
     })
   })
