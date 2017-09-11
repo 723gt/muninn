8d96754c2dae61d3d9144f9978248840d4793450
723 <rb.natsumi@gmail.com>
2017-09-11 15:42:59 +0900

    post のメソッド作成

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET / */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* POST / */
+router.post('/',function(req,res,next){
+  console.log(req.body);
+})
 module.exports = router;
