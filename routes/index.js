ceb325c359739ea5c4c7f71b0e5655687aa1e673
723 <rb.natsumi@gmail.com>
2017-09-13 00:07:38 +0900

    to Red

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,9 @@ var router = express.Router();
 
 /* GET / */
 router.get('/', function(req, res, next) {
-  res.send();
 });
 
 /* POST / */
 router.post('/',function(req,res,next){
-  res.send();
 })
 module.exports = router;
