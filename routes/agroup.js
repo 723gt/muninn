88b2aa748e58374a18b5ba02e12e2e9253999cbf
723 <rb.natsumi@gmail.com>
2017-09-21 14:48:47 +0900

    A班へのPOSTを出来るように実装

--- routes/agroup.js
+++ routes/agroup.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../lib/agroup/database.js');
 
 /* GET /api */
 router.get('/api', function(req, res, next) {
@@ -7,5 +8,12 @@ router.get('/api', function(req, res, next) {
 
 /* POST / */
 router.post('/api',function(req,res,next){
+  let data = req.body;
+  let res_flag = false;
+  if(data.player_name  && data.music && data.scoer){
+    res_flag = db.insertPostData(data);
+  }
+  if(res_flag){res.status(200).end();}
+  else{res.status(400).end();}
 })
 module.exports = router;
