b718b966572ae0f5f051d6457cb44c427355a48d
723 <rb.natsumi@gmail.com>
2017-10-21 22:23:15 +0900

    udp サーバ完成

---
+++ client.js
@@ -0,0 +1,15 @@
+var dgram = require('dgram');
+// const HOST = '150.95.131.81';
+//const HOST = '150.95.131.81';
+const HOST = '127.0.0.1';
+const PORT = 3333;
+let s = process.argv[2]
+var json = `{ "player_name" : "Fuga","music": "Himmel\'s wander","score": ${s}}`
+var msg = new Buffer(json);
+
+sock = dgram.createSocket('udp4');
+
+sock.send(msg,0,msg.length,PORT,HOST,function(err,bytes){
+  if(err){console.log(err)}
+  sock.close();
+})
--- udpserver.js
+++ udpserver.js
@@ -1,8 +1,12 @@
 var PORT = 3333;
 var dgram = require("dgram");
+var db = require("./lib/agroup/database.js");
 
+console.log("UDP Server Start!!");
 sock = dgram.createSocket("udp4",function(msg,info){
-  console.log("msg:" + msg);
+  let data = JSON.parse(msg);
+  console.log("Send Data is:" + msg);
+  db.insertPostData(data);
 })
 
 sock.bind(PORT,'0.0.0.0');
