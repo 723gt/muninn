a00fdb0da08bd51715f673b72a3339ff9b23d186
723 <rb.natsumi@gmail.com>
2017-10-22 04:54:41 +0900

    try catchを実装

--- udpserver.js
+++ udpserver.js
@@ -3,10 +3,15 @@ var dgram = require("dgram");
 var db = require("./lib/agroup/database.js");
 
 console.log("UDP Server Start!!");
-sock = dgram.createSocket("udp4",function(msg,info){
-  let data = JSON.parse(msg);
-  console.log("Send Data is:" + msg);
-  db.insertPostData(data);
-})
+try{
+  sock = dgram.createSocket("udp4",function(msg,info){
+    let data = JSON.parse(msg);
+    console.log("Send Data is:" + msg);
+    db.insertPostData(data);
+  })
 
-sock.bind(PORT,'0.0.0.0');
+  sock.bind(PORT,'0.0.0.0');
+}
+catch(e){
+  console.log(e);
+}
