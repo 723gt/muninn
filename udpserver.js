f2ec6d00097f41dbac17ac5d2e55d2c1de119615
723 <rb.natsumi@gmail.com>
2017-10-22 05:00:09 +0900

    try ry

--- udpserver.js
+++ udpserver.js
@@ -5,9 +5,15 @@ var db = require("./lib/agroup/database.js");
 console.log("UDP Server Start!!");
 try{
   sock = dgram.createSocket("udp4",function(msg,info){
-    let data = JSON.parse(msg);
-    console.log("Send Data is:" + msg);
-    db.insertPostData(data);
+    try{
+      let data = JSON.parse(msg);
+      console.log("Send Data is:" + msg);
+      db.insertPostData(data);
+    }
+    catch(e){
+      console.log(e);
+      console.log("ERROR Massagse is:" + msg)
+    }
   })
 
   sock.bind(PORT,'0.0.0.0');
